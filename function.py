c7bb69cf5027bdd3ed58056bc45ac8cded6d834b
Abdullah Al Noman <165512794+nomancsediu@users.noreply.github.com>
2025-11-25 23:26:52 +0600

    Committed by Noman

--- function.py
+++ function.py
@@ -1,1 +1,56 @@
+# def blender(fruit):
+#     juice = fruit+' juice'
+#     return juice
 
+# result = blender('Mango')
+# ans = blender('Lemon')
+# print(ans)
+# print(result)
+
+# def add(a,b):
+#     total = a+b
+#     return total
+
+# result = add(10,20)
+# print(result)
+
+# def add(a,b,c):
+#     total = a+b
+#     return total
+
+# result = add(10,20,30)
+# print(result)
+
+# def add(a,b):
+#     total = a+b
+#     return total
+
+# result = add(b=10,a=40)
+# print(result)
+
+# def add(a,b=20):
+#     total = a+b
+#     return total
+
+# result = add(10)
+# print(result)
+
+# def add(*args):
+#     print(type(args))
+#     temp = 0
+#     for i in args:
+#         temp+=i
+#     return temp
+
+# result = add(10,20,30,40,50)
+# print(result)
+
+# def add(**kwargs):
+
+#     temp = 0
+#     for i in kwargs:
+#         temp+=kwargs[i]
+#     return temp
+
+# result = add(b=10,a=40,c=89,d=89)
+# print(result)
